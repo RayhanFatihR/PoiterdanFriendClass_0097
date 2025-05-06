61d10586d04a722ac26d901e28114e4ea8763d5d
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-06 14:47:45 +0700

    Tahap 1

---
+++ contoh.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public: //akses modifier
+    int panjang, lebar;
+
+public:
+
+    void input()
+    {
+        cout << "Masukan Panjang: ";
+        cin >> panjang;
+        cout << "Masukan Lebar: ";
+        cin >> lebar;
+    }
+}
